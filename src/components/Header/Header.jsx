b987f1d023e4a92d7a7bfedbd69166a0088c8e61
Govind Gupta <guptagovind1410@gmail.com>
2023-08-04 15:37:44 +0530

    Header Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,50 @@
-import "./Header.scss";
+import { useEffect, useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TbSearch } from 'react-icons/tb';
+import { CgShoppingCart } from 'react-icons/cg';
+import { VscAccount } from 'react-icons/vsc';
+import Search from './Search/Search';
+import { Context } from '../../utils/context';
+import Cart from '../Cart/Cart';
+import './Header.scss';
+
 const Header = () => {
-    return <div>Header</div>;
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offSet = window.scrollY;
+    // console.log(offSet);
+    if (offSet > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <header className={`main-header ${scrolled ? 'sticky-header' : ''}`}>
+      <div className="header-content">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Category</li>
+        </ul>
+        <div className="center">ShopCart</div>
+        <div className="right">
+          <TbSearch />
+          <VscAccount />
+          <span className="cart-icon">
+            <CgShoppingCart />
+            <span>5</span>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
