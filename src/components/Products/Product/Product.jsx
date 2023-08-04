2a23ac8604e67f76789d5a1ffbfe475443c93a6b
Govind Gupta <guptagovind1410@gmail.com>
2023-08-04 21:00:33 +0530

    Category Page Created

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,18 @@
 import "./Product.scss";
+import product from '../../../assets/products/earbuds-prod-2.webp'
+
 const Product = () => {
-    return <div>Product</div>;
+    return (
+        <div className="product-card">
+            <div className="thumbnail">
+                <img src={product} alt="product-img" />
+            </div>
+            <div className="prod-details">
+                <span className="name">Product Name</span>
+                <span className="price">&#8377;599</span>
+            </div>
+        </div>
+        );
 };
 
 export default Product;
