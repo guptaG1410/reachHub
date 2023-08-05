e39dc9a326d7c69a856538e3d7c28da01c7ab979
Govind Gupta <guptagovind1410@gmail.com>
2023-08-05 22:12:31 +0530

    Categories Page API Implemented

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,12 +1,24 @@
+import { useParams } from 'react-router-dom';
 import Products from '../Products/Products';
 import './Category.scss';
+import useFetch from '../../hooks/fetchData';
 
 const Category = () => {
+  const { id } = useParams();
+  const { data } = useFetch(
+    `/api/prdoucts?populate=*&[filters][categories][id]=${id}`
+  );
+
   return (
     <div className="category-main-content">
       <div className="layout">
-        <div className="category-title">Category Title</div>
-        <Products notReq={true}/>
+        <div className="category-title">
+          {
+            data?.data?.[0]?.attributes?.categories?.data?.[0]?.attributes
+              ?.title
+          }
+        </div>
+        <Products notReq={true} products={data}/>
       </div>
     </div>
   );
